9b384fbd4096bb3580d8ba3e04a8c076a3688b78
Boone B Gorges <boonebgorges@gmail.com>
2016-06-03 13:05:01 -0500

    Rewrites for client.

--- classes/Client.php
+++ classes/Client.php
@@ -10,14 +10,19 @@ namespace WeBWorK;
 class Client {
 	public function __construct() {
 		$this->api_client = new \WeBWorK\Client\APIClient();
+		$this->rewrites = new \WeBWorK\Client\Rewrites();
 
 		add_filter( 'the_content', array( $this, 'filter_the_content' ) );
 	}
 
 	public function filter_the_content( $content ) {
-		if ( ! empty( $_GET['do_webwork'] ) ) {
+		$ww_problem = get_query_var( 'ww_problem' );
+		if ( $ww_problem ) {
 			$content = '<div id="webwork-app"></div>';
-			wp_enqueue_script( 'webwork-app', plugins_url() . '/webwork/build/widget.js', array( 'jquery' ) );
+			wp_enqueue_script( 'webwork-app', plugins_url() . '/webwork/build/index.js' );
+			wp_localize_script( 'webwork-app', 'WWData', array(
+				'problem_id' => $ww_problem,
+			) );
 		}
 
 		return $content;

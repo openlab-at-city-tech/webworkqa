2d4375fc09254d37923b88855edf2b32d54e7bd9
Boone B Gorges <boonebgorges@gmail.com>
2016-06-27 20:03:28 -0500

    Remove unused styles in Answered dialog.

--- src/components/AnsweredDialog.js
+++ src/components/AnsweredDialog.js
@@ -4,40 +4,6 @@ export default class AnsweredDialog extends React.Component {
 	render() {
 		const { responseId, onAnsweredClick, isAnswered } = this.props
 
-		var styles = {
-			wwScore: {
-				width: '40px',
-				float: 'left'
-			},
-			arrow: {
-				width: '0px',
-				height: '0px',
-				borderLeft: '20px solid transparent',
-				borderRight: '20px solid transparent',
-			},
-			uparrow: {
-				borderBottom: '20px solid #999'
-			},
-			downarrow: {
-				borderTop: '20px solid #999'
-			},
-			myvoteup: {
-				borderBottom: '20px solid #000'
-			},
-			myvotedown: {
-				borderTop: '20px solid #000'
-			},
-			disabledup: {
-				borderBottom: '20px solid #ddd'
-			},
-			disableddown: {
-				borderTop: '20px solid #ddd'
-			},
-			wwScoreValue: {
-				textAlign: 'center'
-			}
-		}
-
 		return (
 			<div className="ww-answered">
 				<label>

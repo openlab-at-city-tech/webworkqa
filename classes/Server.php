9e825ac996d8c22d0525c0ea2f07bfd183d31c02
Boone B Gorges <boonebgorges@gmail.com>
2016-06-30 08:50:38 -0500

    Porting some POST routing logic from v1.

--- classes/Server.php
+++ classes/Server.php
@@ -8,6 +8,10 @@ namespace WeBWorK;
  * @since 1.0.0
  */
 class Server {
+	protected $post_data;
+	protected $remote_class_url;
+	protected $webwork_user;
+
 	public function __construct() {
 		$this->schema = new Server\Schema();
 		$this->schema->init();
@@ -56,26 +60,53 @@ class Server {
                 if ( empty( $_GET['webwork'] ) || '1' != $_GET['webwork'] ) {
                         return;
                 }
-		if ( ! is_page( 'webwork' ) ) {
-//			return;
+
+		if ( ! empty( $_POST ) ) {
+			$this->set_post_data( $_POST );
+			$this->set_remote_class_url( wp_unslash( $_SERVER['HTTP_REFERER'] ) );
+		} else {
+			if ( isset( $_GET['remote_class_url'] ) ) {
+				$this->set_remote_class_url( wp_unslash( $_GET['remote_class_url'] ) );
+			}
+
+			if ( isset( $_GET['webwork_user'] ) ) {
+				$this->webwork_user = wp_unslash( $_GET['webwork_user'] );
+			}
+
+			if ( $this->webwork_user ) {
+				$key = $this->get_post_data_option_key();
+				$post_data = get_option( $key );
+
+				if ( $post_data ) {
+					$this->set_post_data( $post_data );
+
+					// This data should never be reused across redirects.
+					delete_option( $key );
+				}
+			}
 		}
 
-		if ( ! isset( $_POST ) ) {
-			return;
-		}
+		// Store the submitted post data, so it's available after a redirect.
+		$this->store_post_data();
 
-		// @todo Check permissions against target site - maybe share logic with endpoints.
+		// For the time being, all requests must be authenticated.
+		// @todo Check permissions against client site - maybe share logic with endpoints.
 		if ( ! is_user_logged_in() ) {
-			return;
+			$redirect_url = add_query_arg( array(
+				'webwork' => 1,
+				'webwork_user' => $this->webwork_user,
+				'remote_class_url' => $this->remote_class_url,
+			), home_url() );
+			wp_safe_redirect( wp_login_url( $redirect_url ) );
+			die();
 		}
 
-		$source_parts = $this->sanitize_class_url( $_SERVER['HTTP_REFERER'] );
-		$source = $source_parts['base'];
+		$source = $this->remote_class_url;
 
 		$problem = new Server\Problem();
 
 		// Do not unslash. wp_insert_post() expocts slashed. A nightmare.
-		$pg_object = base64_decode( $_POST['pg_object'] );
+		$pg_object = base64_decode( $this->post_data['pg_object'] );
 
 		// Replace LaTeX backslashes with dummy character, so they aren't stripped.
 		// This will break with literal backslashes?
@@ -113,6 +144,10 @@ class Server {
 		die();
 	}
 
+	public function set_post_data( $data ) {
+		$this->post_data = $data;
+	}
+
 	/**
 	 * Sanitize a remote class URL.
 	 *
@@ -132,7 +167,7 @@ class Server {
 			}
 		}
 
-//		$this->remote_referer_url = $parts['scheme'] . '://' . $parts['host'] . $parts['path'];
+		$this->remote_referer_url = $parts['scheme'] . '://' . $parts['host'] . $parts['path'];
 
 		if ( $subpath && $subpath === substr( $parts['path'], -strlen( $subpath ) ) ) {
 			$base = substr( $parts['path'], 0, -strlen( $subpath ) );
@@ -159,6 +194,19 @@ class Server {
 		return $retval;
 	}
 
+	/**
+	 * Set the course URL for the request.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $remote_class_url
+	 */
+	public function set_remote_class_url( $remote_class_url ) {
+		$url_parts = $this->sanitize_class_url( $remote_class_url );
+		$this->remote_class_url = $url_parts['base'];
+		$this->webwork_user = $url_parts['user'];
+	}
+
 	protected function get_client_from_course_url( $course_url ) {
 		// @todo We need a better way to do this.
 		$clients = get_option( 'webwork_clients', array() );
@@ -169,5 +217,56 @@ class Server {
 		}
 
 		return (int) $client;
+	}
+
+	/**
+	 * Get the key to be used when storing the POST data in the options table.
+	 *
+	 * @todo Does this need to use a timestamp? Seems like probably not?
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $args {
+	 *     @type string $ip   IP address. Falls back on REMOTE_ADDR.
+	 *     @type string $user WW user name. Falls back on $this->webwork_user.
+	 * }
+	 * @return string
+	 */
+	protected function get_post_data_option_key( $args = array() ) {
+		if ( isset( $args['ip'] ) ) {
+			$ip = $args['ip'];
+		} elseif ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
+			$ip = wp_unslash( $_SERVER['REMOTE_ADDR'] );
+		} else {
+			$ip = '';
+		}
+
+		if ( isset( $args['user'] ) ) {
+			$user = $args['user'];
+		} else {
+			$user = $this->webwork_user;
+		}
+
+		// If neither $ip or $user is available, don't store the data.
+		if ( ! $ip && ! $user ) {
+			return false;
+		}
+
+		return 'webwork_post_data_' . md5( $ip . $user );
+	}
+
+	/**
+	 * Store POST and other data that will be needed after redirect.
+	 *
+	 * @since 1.0.0
+	 */
+	public function store_post_data() {
+		$this->post_data_key = $this->get_post_data_option_key();
+
+		// Store the remote class URL for later use.
+		$this->post_data['remote_class_url']   = $this->remote_class_url;
+		$this->post_data['remote_referer_url'] = $this->remote_referer_url;
+
+		update_option( $this->post_data_key, $this->post_data );
 	}
 }

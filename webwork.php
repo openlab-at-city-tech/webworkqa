7fa919a9ec427a289756d6e27e690022b7876325
Boone B Gorges <boonebgorges@gmail.com>
2016-07-20 13:08:25 -0500

    Require WP-API.

--- webwork.php
+++ webwork.php
@@ -31,6 +31,11 @@ function webwork_init() {
 		return;
 	}
 
+	if ( ! class_exists( 'WP_REST_Controller' ) ) {
+		add_action( 'admin_notices', create_function( '', "echo '<div class=\"error\"><p>" . __( 'WeBWorK for WordPress requires the WP-API plugin to function properly. Please install WP-API or deactivate WeBWorK for WordPress.', 'webwork' ) . "</p></div>';" ) );
+		return;
+	}
+
 	spl_autoload_register( 'webwork_autoload_register' );
 
 	$GLOBALS['webwork'] = \WeBWorK\Loader::init();

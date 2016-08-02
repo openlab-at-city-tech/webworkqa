12ade018563dd564a840b8d2e85ce7aa0463fe1f
Boone B Gorges <boonebgorges@gmail.com>
2016-08-02 16:19:52 -0500

    More dynamic boilerplate for Results vs Index.

--- src/components/QuestionIndex.js
+++ src/components/QuestionIndex.js
@@ -10,18 +10,36 @@ export default class QuestionIndex extends Component {
 
 		const headerElement = isResultsPage ? <ResultsHeaderContainer /> : ''
 
+		let introElement = ''
+		if ( isResultsPage ) {
+			introElement = (
+				<div className="index-intro">
+					<p>Brief explanation of results?</p>
+				</div>
+			)
+		} else {
+			introElement = (
+				<div className="index-intro">
+					<h2>Introduction</h2>
+					<p>Quaerat nemo debitis dolorum ratione est exercitationem aut molestias. Excepturi beatae et autem et quia quo rem. Et provident id ducimus. Quaerat temporibus doloribus rerum eaque et. Odio necessitatibus eos vitae molestiae in. Numquam et et molestias velit mollitia consequatur reiciendis. Iusto quo maxime quibusdam libero. Recusandae porro maiores laudantium mollitia rerum cumque voluptatem delectus. Sit quibusdam sint animi. Nemo qui qui expedita.</p>
+				</div>
+			)
+		}
+
+		let listHeaderElement = ''
+		if ( ! isResultsPage ) {
+			listHeaderElement = <h2>Recent Activity</h2>
+		}
+
 		return (
 			<div>
 				{headerElement}
 
 				<div className="ww-main problem-index">
-					<div className="index-intro">
-						<h2>Introduction</h2>
-						<p>Quaerat nemo debitis dolorum ratione est exercitationem aut molestias. Excepturi beatae et autem et quia quo rem. Et provident id ducimus. Quaerat temporibus doloribus rerum eaque et. Odio necessitatibus eos vitae molestiae in. Numquam et et molestias velit mollitia consequatur reiciendis. Iusto quo maxime quibusdam libero. Recusandae porro maiores laudantium mollitia rerum cumque voluptatem delectus. Sit quibusdam sint animi. Nemo qui qui expedita.</p>
-					</div>
+					{introElement}
 
 					<div className="index-list">
-						<h2>Recent Questions on OpenLab WeBWorK</h2>
+						{listHeaderElement}
 						<QuestionIndexListContainer />
 					</div>
 				</div>

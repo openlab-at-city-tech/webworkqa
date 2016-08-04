7612ae10d22d96904176bc183286ab976078fd9c
Boone B Gorges <boonebgorges@gmail.com>
2016-08-04 17:13:19 -0500

    Filter is_admin check for OpenLab.

--- includes/openlab.php
+++ includes/openlab.php
@@ -14,3 +14,13 @@ function openlab_webwork_author_type_label( $label, $user_id ) {
 	return $label;
 }
 add_filter( 'webwork_author_type_label', 'openlab_webwork_author_type_label', 10, 2 );
+
+function openlab_user_is_admin( $is_admin ) {
+	$account_type = xprofile_get_field_data( 'Account Type', $user_id );
+	if ( $account_type && 'Faculty' == $account_type ) {
+		$account_type = true;
+	}
+
+	return $account_type;
+}
+add_filter( 'webwork_user_is_admin', 'openlab_user_is_admin' );

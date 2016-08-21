8b217037f41afc75a2206fcb518943cdfb169c8b
Boone B Gorges <boonebgorges@gmail.com>
2016-08-20 21:00:35 -0500

    Styling updates for sidebar filters.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,9 +6,11 @@ export default class Sidebar extends Component {
 	render() {
 		return (
 			<div className="ww-sidebar">
-				<h3>Explore Questions</h3>
+				<h3 className="ww-header">Explore Questions</h3>
 
 				<div className="ww-sidebar-widget">
+					<p>Voluptatibus eos ipsa consequatur. Dolorem vel amet veritatis totam quia ducimus. Sed praesentium sit illum quasi.</p>
+
 					<ul className="ww-question-filters">
 						<div className="ww-filter-section">
 							<SidebarFilterContainer

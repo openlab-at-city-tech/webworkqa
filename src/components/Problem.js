572c7e03373601503555d13a2bf4a223f4e2b46c
Boone B Gorges <boonebgorges@gmail.com>
2016-08-22 13:20:11 -0500

    Revert "Don't show sort dropdown on single problem."
    This reverts commit 7d0937acbd3460954d7823b6114bb2afbbd79fc0.

--- src/components/Problem.js
+++ src/components/Problem.js
@@ -31,6 +31,10 @@ export default class Problem extends Component {
 				{questionFormElement}
 
 				<div className="problem-questions">
+					<QuestionSortDropdownContainer
+					  itemType='problem'
+					  problemId={problemId}
+					/>
 					<QuestionList questionsById={questionsById} />
 				</div>
 			</div>

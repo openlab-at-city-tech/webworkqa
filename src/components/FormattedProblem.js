56994074dff89abeaf17c742f4d99d50d177140e
Boone B Gorges <boonebgorges@gmail.com>
2016-09-19 16:39:35 -0500

    Rework the way that LaTeX items are rendered.
    In order to support arbittary markup from WeBWorK, we have to be able
    to pass HTML directly to WeBWorK. This doesn't work with the previous
    method of LaTeX parsing, since it required that the non-LaTeX chunks
    appearing between LaTeX chunks be rendered within a single HTML
    wrapper element. But this can't work when the LaTeX is nested inside
    of another element, because open and close tags for the containing
    element get improperly nested in the dummy `span`.
    
    The only solution is to stop rendering LaTeX bits as their own
    components, and instead building a single string for the content of the
    FormattedProblem component.
    
    Sanitization will have to handle server-side, via `wp_kses()`. More to
    come in a subsequent changeset.
    
    See #32.

--- src/components/FormattedProblem.js
+++ src/components/FormattedProblem.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import LaTeX from './LaTeX'
 
 const FormattedProblem = React.createClass( {
 	getDefaultProps: function() {
@@ -19,52 +18,43 @@ const FormattedProblem = React.createClass( {
 			return ( <span></span> )
 		}
 
-		const texRegExp = /(\{\{\{LATEX_DELIM_((?:DISPLAY)|(?:INLINE))_OPEN\}\}\})([^]*?)(\{\{\{LATEX_DELIM_\2_CLOSE\}\}\})/gm
-		const parts = content.split( texRegExp )
-		let children = []
-		let texIndex = 0, texId, typeIndex, mathIndex, closeIndex, display;
-		for ( var i = 0; i < parts.length; i++ ) {
-			if ( i == typeIndex || i == mathIndex || i == closeIndex ) {
-				continue
+		const texRegExp = /\{\{\{LATEX_DELIM_((?:DISPLAY)|(?:INLINE))_((?:OPEN)|(?:CLOSE))\}\}\}/gm
+		let markup = ''
+		let toQueue = []
+
+		markup = content.replace( texRegExp, function( delim, mode, openOrClose ) {
+			if ( 'CLOSE' == openOrClose ) {
+				return '</script>'
 			}
 
-			typeIndex = mathIndex = closeIndex = display = null;
-			if ( parts[i] == '{{{LATEX_DELIM_DISPLAY_OPEN}}}' || parts[i] === '{{{LATEX_DELIM_INLINE_OPEN}}}' ) {
-				texIndex = texIndex + 1
-				texId = 'tex-' + texIndex
+			let typeAttr = 'math/tex'
+			if ( 'DISPLAY' == mode ) {
+				typeAttr += ';mode=display'
+			}
 
-				typeIndex = i + 1
-				mathIndex = i + 2
-				closeIndex = i + 3
+			if ( ! document.hasOwnProperty( 'latexIncrementor' ) ) {
+				document.latexIncrementor = 1;
+			}
 
-				display = 'DISPLAY' == parts[typeIndex] ? 'block' : 'inline'
+			let cssId = 'latex-' + document.latexIncrementor
+			toQueue.push( cssId )
 
-				children.push(
-					<LaTeX
-					  key={texId}
-					  mathKey={texId}
-					  itemId={itemId}
-					  math={parts[mathIndex]}
-					  display={display}
-					  isVisible={isVisible}
-					/>
-				)
-			} else {
-				children.push(
-					<span
-					  key={i}
-					  className='preserve-whitespace'
-					>
-						{parts[i]}
-					</span>
-				)
+			document.latexIncrementor = document.latexIncrementor + 1
+
+			return '<script type="' + typeAttr + '" id="' + cssId + '">'	
+		} )
+
+		if ( window.hasOwnProperty( 'MathJax' ) && window.MathJax.hasOwnProperty( 'Hub' ) ) {
+			for ( var i = 0; i <= toQueue.length; i++ ) {
+				window.MathJax.Hub.Queue(["Update", window.MathJax.Hub, toQueue[i] ]);
 			}
 		}
 
 		return (
-			<div className="formatted-problem">
-				{children}
-			</div>
+			<div 
+			  className="formatted-problem"
+			  dangerouslySetInnerHTML={{__html: markup}}
+			/>
 		)
 	}
 } )

e0bbef3ff8aa8de00b6b57982378b3337291db6f
Boone B Gorges <boonebgorges@gmail.com>
2019-04-29 14:55:06 -0500

    Link to login on instructor notification of question reply.
    See #157.

--- classes/Server/Response.php
+++ classes/Server/Response.php
@@ -292,13 +292,15 @@ class Response implements Util\SaveableAsWPPost, Util\Voteable {
 		$email->set_recipient( $instructor_email );
 		$email->set_subject( sprintf( __( '%1$s has replied to a question in the course %2$s', 'webwork' ), $response_author->display_name, $section ) );
 
+		$link_url = wp_login_url( $this->question->get_url( $this->get_client_url() ) );
+
 		$message = sprintf(
 			__( '%1$s has replied to a question in your course %2$s.
 
 To read and reply, visit %3$s.', 'webwork' ),
 			$response_author->display_name,
 			$section,
-			$this->question->get_url( $this->get_client_url() )
+			$link_url
 		);
 		$email->set_message( $message );
 

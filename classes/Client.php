e4ba9bc981f54f72239b62d01618e9eab1e62d45
Boone B Gorges <boonebgorges@gmail.com>
2020-03-02 13:21:22 -0600

    Revert "More abstraction for markup generation."
    This reverts commit ad3e6605b031bed72cbd07de18df38436c3074ac.

--- classes/Client.php
+++ classes/Client.php
@@ -37,12 +37,6 @@ class Client {
 		wp_enqueue_script( 'webwork-app', plugins_url() . '/webwork/build/index.js', $deps );
 		wp_set_script_translations( 'webwork-app', 'webwork' );
 
-		if ( is_page( 'webwork' ) ) {
-			$ww_problem = true;
-		} else {
-			$ww_problem = get_query_var( 'ww_problem' );
-		}
-
 		$route_base = get_option( 'home' );
 		$route_base = preg_replace( '|https?://[^/]+/|', '', $route_base );
 
@@ -159,5 +153,95 @@ class Client {
 		$markup .= '</div>';
 
 		return $markup;
+	}
+
+	public function filter_the_content( $content ) {
+		return $content;
+		$ww_problem = false;
+		if ( is_page( 'webwork' ) ) {
+			$ww_problem = true;
+		} else {
+			$ww_problem = get_query_var( 'ww_problem' );
+		}
+
+		if ( $ww_problem ) {
+			$content = '<div id="webwork-app"></div>';
+			wp_enqueue_script( 'webwork-app', plugins_url() . '/webwork/build/index.js', array( 'wp-i18n' ) );
+
+			$route_base = get_option( 'home' );
+			$route_base = preg_replace( '|https?://[^/]+/|', '', $route_base );
+
+			// @todo Centralize this logic.
+			$main_site_url = get_blog_option( 1, 'home' );
+			$rest_api_endpoint = trailingslashit( $main_site_url ) . 'wp-json/webwork/v1/';
+
+			// @todo Abstract.
+			$post_data       = null;
+			$ww_problem_text = '';
+			if ( ! empty( $_GET['post_data_key'] ) ) {
+				$server_site_id = apply_filters( 'webwork_server_site_id', 1 );
+				$post_data = get_blog_option( 1, $_GET['post_data_key'] );
+
+				$ww_problem_text = base64_decode( $post_data['pg_object'] );
+			}
+
+			// @todo This is awful.
+			$clients = get_blog_option( 1, 'webwork_clients' );
+			$remote_course_url = array_search( get_current_blog_id(), $clients );
+
+			wp_localize_script( 'webwork-app', 'WWData', array(
+				// Problem config.
+				'problem_id'             => $ww_problem,
+				'problem_text'           => $ww_problem_text,
+				'remote_course_url'      => $remote_course_url,
+
+				// App config.
+				'rest_api_nonce'         => wp_create_nonce( 'wp_rest' ),
+				'rest_api_endpoint'      => $rest_api_endpoint,
+				'route_base'             => trailingslashit( $route_base ) . 'webwork/',
+
+				// Permissions.
+				'user_can_ask_question'  => is_user_logged_in(), // todo
+				'user_can_post_response' => is_user_logged_in(), // todo
+				'user_can_vote'          => is_user_logged_in(), // todo
+			) );
+
+			wp_enqueue_style( 'webwork-app', plugins_url() . '/webwork/assets/css/app.css' );
+
+			wp_register_script( 'webwork-mathjax-loader', WEBWORK_PLUGIN_URL . '/assets/js/webwork-mathjax-loader.js' );
+
+			$webwork_mathjax_loader_strings = array(
+				'mathjax_src' => esc_url( 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_HTMLorMML-full' ),
+			);
+			wp_localize_script( 'webwork-mathjax-loader', 'WeBWorK_MathJax', $webwork_mathjax_loader_strings );
+
+			wp_enqueue_script( 'webwork-mathjax-loader' );
+		}
+
+		return $content;
+	}
+
+	public function filter_login_message( $message ) {
+		if ( empty( $_GET['is-webwork-redirect'] ) ) {
+			return $message;
+		}
+
+		$site_name = get_option( 'blogname' );
+		$message = sprintf(
+			esc_html__( 'You have been directed to %s from WeBWorK. Before posting a question, you must log in using your %s credentials.' ),
+			esc_html( $site_name ),
+			esc_html( $site_name )
+		);
+
+		/**
+		 * Filters the WeBWorK login redirect message.
+		 *
+		 * @param string $message
+		 */
+		$message = apply_filters( 'webwork_login_redirect_message', $message );
+
+		$retval = '<p class="message">' . $message . '</p>';
+
+		return $retval;
 	}
 }
